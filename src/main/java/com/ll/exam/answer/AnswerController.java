61e6c54228fd57322b0746bcb1e45776062df548
jiooong <lucy991010@gmail.com>
2022-08-22 01:12:21 +0900

    답변 등록 버튼, 답변 컨트롤러 만들기

---
+++ src/main/java/com/ll/exam/answer/AnswerController.java
@@ -0,0 +1,27 @@
+package com.ll.exam.answer;
+
+import com.ll.exam.question.Question;
+import com.ll.exam.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+
+    public final QuestionService questionService;
+
+
+
+    @PostMapping ("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content){
+        Question question = this.questionService.getQuestion(id);
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
