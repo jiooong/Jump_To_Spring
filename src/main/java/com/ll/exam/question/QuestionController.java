b86562e19f989843f32d21be7a52d80933b2d99f
jiooong <lucy991010@gmail.com>
2022-08-21 23:35:12 +0900

    질문 상세 컨트롤러 만들기, 서비스 수정, 템플릿 수정

--- src/main/java/com/ll/exam/question/QuestionController.java
+++ src/main/java/com/ll/exam/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,5 +21,12 @@ public class QuestionController {
         List<Question> questionList=this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @RequestMapping(value="/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){ //Model객체는 자바 클래스와 템플릿 간의 연결고리 역할
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
