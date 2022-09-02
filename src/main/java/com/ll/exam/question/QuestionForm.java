2ab2d65ee0ea5eb293e7e5d9e446869e3050785b
jiooong <lucy991010@gmail.com>
2022-09-02 23:34:50 +0900

    입력 값 검증을 위해 폼 클래스 추가

---
+++ src/main/java/com/ll/exam/question/QuestionForm.java
@@ -0,0 +1,19 @@
+package com.ll.exam.question;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionForm {
+    @NotEmpty(message="제목은 필수항목입니다.")
+    @Size(max=200)
+    private String subject;
+
+    @NotEmpty(message="내용은 필수항목입니다.")
+    private String content;
+}
+//화면에서 전달되는 입력 값을 검증하기 위해 폼 클래스가 필요
