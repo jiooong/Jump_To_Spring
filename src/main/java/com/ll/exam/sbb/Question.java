993b134def0aee2a2ed4b77696267c90244a2357
jiooong <lucy991010@gmail.com>
2022-09-22 23:56:12 +0900

    Feat: Question 클래스작성

---
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -0,0 +1,31 @@
+package com.ll.exam.sbb;
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+
+    private LocalDateTime createDate;
+}
