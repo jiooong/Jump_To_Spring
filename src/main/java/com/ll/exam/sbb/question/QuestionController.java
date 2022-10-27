d320dd47f196f1e1a8e0cb386c0bd7ae0ead5707
jiooong <lucy991010@gmail.com>
2022-10-28 02:59:05 +0900

    feat : questionlist 템플릿 추가

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -8,10 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class QuestionController {
 
     @RequestMapping("/question/list")
-    @ResponseBody
     public String list(){
-        return "question list"; // 해당코드는 문자열을 직접 자바 코드로 작성하여 브라우저에 리턴
-        //보통 자바 코드를 삽입할 수 있는 HTML형식의 파일인 템플릿 방식을 사용한다
+        return "question_list";
     }
 
 }
