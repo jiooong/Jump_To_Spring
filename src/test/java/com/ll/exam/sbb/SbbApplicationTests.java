50e054fa6f8dfa852d220738e58969153c60ba10
jiooong <lucy991010@gmail.com>
2022-10-28 02:43:25 +0900

    test : 답변 생성하기

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -17,16 +17,21 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
+	@Autowired
+	private AnswerRepository answerRepository;
+
 	@Test
 	void testJpa() {
 
-		assertEquals(3, this.questionRepository.count());
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-		this.questionRepository.delete(q);
-		assertEquals(2, this.questionRepository.count());
+  	 	Optional<Question> oq = this.questionRepository.findById(3);
+	 	assertTrue(oq.isPresent());
+	 	Question q = oq.get();
 
+	 	Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
 	}
 
 
