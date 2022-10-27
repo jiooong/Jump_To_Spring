51c9fe07755401726ca2a39e60f2a6aaa7263a8b
jiooong <lucy991010@gmail.com>
2022-10-28 03:04:55 +0900

    feat : QuestionControlller에 model객체 추가

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,15 +1,22 @@
 package com.ll.exam.sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
-
+    private QuestionRepository questionRepository;
     @RequestMapping("/question/list")
-    public String list(){
+    public String list(Model model){
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
+    //자바 클래스와 템플릿 간의 연결고리 역할을 하는 model 객체
 
 }
