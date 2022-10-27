061458997d6b643f51c20630c08ccfa6579ca7bc
jiooong <lucy991010@gmail.com>
2022-10-28 03:34:44 +0900

    feat: Root Url redirect연결

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //서버에 전달된 클라이언트의 요청을 처리하는 자바 클래스
 public class MainController {
+
+    @RequestMapping("/")
+    public String root(){
+        return "redirect:/question/list";
+    }
+    //redirect url은 완전히 새로운 url로 요청. forward url은 기존 요청 값들이 유지된 상태로 전환
+
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(){
